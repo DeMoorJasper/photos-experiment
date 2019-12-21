ca63607b1783c29be051da1cd355de5dab32179f
Jasper De Moor <jasperdemoor@gmail.com>
2019-12-21 16:20:24 +0100

    tweak api

--- src/js/utils/api.tsx
+++ src/js/utils/api.tsx
@@ -1,32 +1,40 @@
 import { getRandomName, getRandomInt } from "./random";
 
+let posts: Array<any> = [];
+
 export async function getPosts(start: number, end: number) {
   await new Promise(resolve => {
     setTimeout(resolve, 150);
   });
 
-  return new Array(end - start).fill("").map((item, index) => {
-    let imageId = getRandomInt(1, 15);
+  if (posts.length < end) {
+    posts.push(
+      ...new Array(end - posts.length).fill("").map((_, index) => {
+        let imageId = getRandomInt(1, 15);
 
-    return {
-      imageUri: `https://storage.googleapis.com/instagram-clone-test-images/original/${imageId}.jpg`,
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-      comments: [
-        {
-          user: getRandomName(),
-          comment:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
-        }
-      ],
-      user: {
-        name: getRandomName()
-      },
-      likes: 10,
-      liked: true,
-      onLike: () => {}
-    };
-  });
+        return {
+          id: posts.length + index,
+          imageUri: `https://storage.googleapis.com/instagram-clone-test-images/original/${imageId}.jpg`,
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+          comments: [
+            {
+              user: getRandomName(),
+              comment:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+            }
+          ],
+          user: {
+            name: getRandomName()
+          },
+          likes: 10,
+          liked: true
+        };
+      })
+    );
+  }
+
+  return posts.slice(start, end);
 }
 
 export default async function fetcher(uri: string) {
