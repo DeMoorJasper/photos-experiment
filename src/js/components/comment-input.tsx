a3bbfcffd6a24a92b067035df961528ee89b7668
Jasper De Moor <jasperdemoor@gmail.com>
2020-01-01 14:54:34 +0100

    use atlaskit editor

--- src/js/components/comment-input.tsx
+++ src/js/components/comment-input.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Editor from '@atlaskit/editor-core';
+import { Editor } from "@atlaskit/editor-core";
 
 type Props = {
   onSubmit: (comment: string) => any;
@@ -16,9 +16,6 @@ export default function CommentInput(props: Props) {
     setValue(e.target.value);
   };
 
-  Editor.ACTION;
-  console.log(Editor);
-
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       onSubmit(value);
@@ -26,14 +23,14 @@ export default function CommentInput(props: Props) {
     }
   };
 
-  return (
-    <input
-      type="text"
-      className="bg-gray-100 text-gray-900 shadow w-full p-2"
-      placeholder="Comment..."
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-      value={value}
-    />
-  );
+  /*return <input
+    type="text"
+    className="bg-gray-100 text-gray-900 shadow w-full p-2"
+    placeholder="Comment..."
+    onChange={handleChange}
+    onKeyDown={handleKeyDown}
+    value={value}
+  />;*/
+
+  return <Editor appearance="comment" />;
 }
