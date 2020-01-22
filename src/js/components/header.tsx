64b9eb7576f0754fb86a9580452972c60d770972
Jasper De Moor <jasperdemoor@gmail.com>
2020-01-22 22:14:53 +0100

    fix padding

--- src/js/components/header.tsx
+++ src/js/components/header.tsx
@@ -13,7 +13,7 @@ export default function Header(props: Props) {
   });
 
   return (
-    <header className="py-4 border-gray-200 border-b shadow">
+    <header className="p-4 border-gray-200 border-b shadow">
       <div className="flex justify-between max-w-xl mx-auto items-center">
         <span className="text-xl font-semibold">Social Pixi</span>
         <span className="font-medium">{userResponse.data.name}</span>
